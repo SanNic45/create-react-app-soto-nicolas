f7c06e9c0a0937edf43d0ce2c34a3cdc0f41af60
SanNic45 <119127179+SanNic45@users.noreply.github.com>
2023-11-24 20:38:12 -0500

    Update App.js
    Cambios español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Tu primer React
+          Editar <code>src/App.js</code> Tu primer React
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Edúcate con React
         </a>
       </header>
     </div>
